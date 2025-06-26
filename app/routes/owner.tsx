136670ea679d7c6c986453fbb189301d03d98c6b
Chris Mendoza <chris@getformalize.com>
2025-06-25 17:15:39 -0700

    add mock owners, load one that matches ID and show details

--- app/routes/owner.tsx
+++ app/routes/owner.tsx
@@ -2,16 +2,50 @@ import type { Route } from "./+types/owner";
 // import { useFetcher } from "react-router";
 // import type { loader } from "./search";
 
+interface Owner {
+    id: number,
+    name: string,
+    phoneNumber: string,
+}
+const OWNERS_MOCK: Owner[] = [
+    {
+        id: 1,
+        name: "Angel M",
+        phoneNumber: "+18777804236"
+    },
+    {
+        id: 2,
+        name: "Chris M",
+        phoneNumber: "+12092704012"
+    }
+];
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Owner" },
   ];
 }
 
-export default function Owner({ params }: Route.ComponentProps) {
-    return (
-        <body>
-            <h1>Owner ({params.ownerId})</h1>
-        </body>
-      )
+export async function loader({ params }: Route.LoaderArgs) {
+    const owner = OWNERS_MOCK.find(owner => owner.id === Number(params.ownerId));
+
+    return { owner };
+}
+
+export default function Owner({ loaderData, params }: Route.ComponentProps) {
+    const { owner } = loaderData;
+
+    if(owner) {
+        return (
+            <body>
+                <h1>Owner</h1>
+                <h2>{owner.name}</h2>
+                <h2>{owner.phoneNumber}</h2>
+            </body>
+        )
+    }
+
+    return <body>
+        <h1>Owner with ID {params.ownerId} is missing</h1>
+    </body>
   }
