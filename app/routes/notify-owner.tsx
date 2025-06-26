10f2a252ac201aebda5628fce9d290fd4deddd2b
Chris Mendoza <chris@getformalize.com>
2025-06-25 18:07:36 -0700

    import Owner mocks, find in loader, send SMS with owner and pet details

--- app/routes/notify-owner.tsx
+++ app/routes/notify-owner.tsx
@@ -1,22 +1,46 @@
-// import twilio from "twilio";
+import twilio from "twilio";
+
+import { findOwnerById } from "~/mocks/owners";
+
 
 export async function loader({ request }: { request: Request}) {
     let url = new URL(request.url);
     let ownerId = url.searchParams.get("ownerId");
     let missingPetId = url.searchParams.get("missingPetId");
 
-    // const accountSid = process.env.TWILIO_ACCOUNT_SID;
-    // const authToken = process.env.TWILIO_AUTH_TOKEN;
-    // const twilioAPI = twilio(accountSid, authToken);
+    if(!ownerId) {
+        return { message: "Owner ID was not provided in request form data."}
+    }
+    if(!missingPetId) {
+        return { message: "Missing pet ID was not provided in request form data."}
+    }
 
-    // const fromPhoneNumber = "+18666257307";
-    // const toPhoneNumber = "+18777804236";
+    const owner = findOwnerById(ownerId);
 
-    // const message = await twilioAPI.messages.create({
-    //   body: "From Pet Directory Admin Console - Will be 'Found pet' notification",
-    //   from: fromPhoneNumber,
-    //   to: toPhoneNumber,
-    // });
-  
+    if(!owner) {
+        return { message: `Owner with ID, ${ownerId}, was not found.`}
+    }
+
+    if(owner.missingPet === null || owner.missingPet.id !== Number(missingPetId)) {
+        return { message: "No missing pet tied to user, or it didn't match the provided Missing Pet ID"}
+    }
+
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const twilioAPI = twilio(accountSid, authToken);
+
+    const fromPhoneNumber = "+18666257307";
+    const toPhoneNumber = owner.phoneNumber;
+
+    const message = await twilioAPI.messages.create({
+        body: `Hello ${owner.name},
+            You're pet ${owner.missingPet.name} has been found!
+            He is being looked after at this facility:
+            [Insert facility details]
+        `,
+        from: fromPhoneNumber,
+        to: toPhoneNumber,
+    });
+
     return { message: `Notify owner (ID: ${ownerId}) via SMS that pet with ID ${missingPetId} was found!` };
 }
