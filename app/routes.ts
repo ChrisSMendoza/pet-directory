361e7c04ca4b5ecb9c6da76470e36ff2d08b7cb2
Chris Mendoza <chris@getformalize.com>
2025-06-26 02:11:50 -0700

    remove admin and search since happens in owner page

--- app/routes.ts
+++ app/routes.ts
@@ -3,8 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
 
-    route("admin", "routes/admin.tsx"),
-    route("search", "routes/search.tsx"),
     route("owners/:ownerId", "routes/owner.tsx"),
     route("notify", "routes/notify-owner.tsx")
 ] satisfies RouteConfig;
